c273b6260c9028b7bfc514e686e899805eb93bc0
maheshwari <perumandlamahi@gmail.com>
2022-03-10 00:11:08 +0530

    initial commit

---
+++ random num game.c
@@ -0,0 +1,38 @@
+random num game.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int num,guess,nguesses=1;
+    srand(time(0));
+    num=rand()%100+1;
+    // printf("%d\n",num);
+    do{
+        printf("guess the num btw 1 to 100");
+        scanf("%d",&guess);
+        if(guess>num){
+            printf("lower num please\n");
+             }
+             else if(guess<num){
+             printf("higher num please\n");
+             }
+             else
+             {
+                 printf("u guessed it in %d attempts\n",nguesses);
+             }
+             
+              nguesses++; 
+      }
+              while(guess!=num);
+               
+             
+             
+        
+    
+        
+     printf("%d",num);   
+    
+    return 0;
+}
+
