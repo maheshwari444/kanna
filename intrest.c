a78149cd479ad12d50e5d7e75f24b3de17dd06ef
maheshwari <perumandlamahi@gmail.com>
2022-03-09 22:34:31 +0530

    finding intrest

---
+++ intrest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int principal=100,rate=4,years=1;
+    int simpleinst=(principal*rate*years)/100;
+    printf("the value is%d",simpleinst);
+    
+    return 0;
+}
